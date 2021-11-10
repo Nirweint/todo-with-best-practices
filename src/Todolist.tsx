c7d67a11f3f8a9a81f9c74cc36041520cfdc43fc
Nirweint <nirweint_1@mail.ru>
2021-11-10 23:40:55 +0300

    changed to many todolists

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,45 +1,28 @@
 import React, {useState} from 'react';
-import {FilterValuesType} from './App';
+import {FilterValuesType, TaskType} from './App';
 import {Button} from "./components/Button";
 import {Input} from "./components/Input";
 
-type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
 
 type PropsType = {
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
-    addTask: (title: string) => void
+    todolistId: string
+    removeTask: (taskId: string, todolistId: string) => void
+    addTask: (title: string, todolistId: string) => void
+    changeFilter: (value: FilterValuesType, todolistId: string) => void
 }
 
 export function Todolist(props: PropsType) {
 
     let [title, setTitle] = useState("")
-    let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    let tasksForTodolist = props.tasks;
-
-    if (filter === "active") {
-        tasksForTodolist = props.tasks.filter(t => !t.isDone);
-    }
-    if (filter === "completed") {
-        tasksForTodolist = props.tasks.filter(t => t.isDone);
-    }
-
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value);
-    }
 
     const callBackHandlerForAddTask = () => {
-        props.addTask(title)
+        props.addTask(title, props.todolistId)
         setTitle("")
     }
     const onClickHandler = (tId: string) => {
-        props.removeTask(tId)
+        props.removeTask(tId, props.todolistId)
     }
 
     return <div>
@@ -52,7 +35,7 @@ export function Todolist(props: PropsType) {
         <Button callBack={callBackHandlerForAddTask} name={"+"}/>
         <ul>
             {
-                tasksForTodolist.map(t => {
+                props.tasks.map(t => {
                     return <li key={t.id}>
                         <input type="checkbox" checked={t.isDone}/>
                         <span>{t.title}</span>
@@ -65,13 +48,13 @@ export function Todolist(props: PropsType) {
         </ul>
         <div>
             <Button name={'all'} callBack={() => {
-                changeFilter('all')
+                props.changeFilter('all', props.todolistId)
             }}/>
             <Button name={'active'} callBack={() => {
-                changeFilter('active')
+                props.changeFilter('active', props.todolistId)
             }}/>
             <Button name={'completed'} callBack={() => {
-                changeFilter('completed')
+                props.changeFilter('completed', props.todolistId)
             }}/>
         </div>
     </div>
